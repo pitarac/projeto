fe99c7a211f4d149767643024d9116e894c7f965
Paulo Henrique Lima Leocádia <39973636+pitarac@users.noreply.github.com>
2023-11-25 22:03:01 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 require('dotenv').config();
-
-
-
 const express = require('express');
 const exphbs = require('express-handlebars');
 const passport = require('passport');
@@ -11,19 +8,13 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const Sequelize = require('sequelize');
 const db = require('./db/connection');
-const User = require('./models/User'); // Modelo de usuário
+const User = require('./models/User');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware para verificar se o usuário está autenticado
-function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next(); // Avança para a próxima etapa
-    }
-    res.redirect('/login'); // Redireciona para a página de login se não estiver autenticado
-}
+require('./models/modelAssociations');
 
 // Configuração da sessão
 app.use(session({
@@ -84,71 +75,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Conexão com o banco de dados
 db.authenticate()
-    .then(() => console.log('Conectou ao banco com sucesso'))
-    .catch(err => console.log('Ocorreu um erro ao conectar', err));
+  .then(() => console.log('Conectou ao banco com sucesso'))
+  .catch(err => console.log('Ocorreu um erro ao conectar', err));
 
 // Rotas
 require('./routes/auth')(app);
+const trocavagasRoutes = require('./routes/trocavagas');
+app.use('/trocavagas', trocavagasRoutes);
 
-// Rota principal (Redirecionamento para login se não estiver autenticado)
-app.get('/', ensureAuthenticated, (req, res) => {
-    // Renderizar a página principal aqui após a autenticação
-    res.render('home');
-});
-
-// Rota de login
-app.get('/login', (req, res) => {
-    res.render('login');
-});
-
-app.post('/login', passport.authenticate('local', {
-    failureRedirect: '/login', // Redireciona para o login se falhar
-    failureFlash: false
-}), (req, res) => {
-    res.redirect('/'); // Redireciona para a página inicial se o login for bem-sucedido
-});
-
-// Rota de logout
-app.get('/logout', (req, res) => {
-    req.logout();
-    res.redirect('/login');
-});
-
-// Rota de registro
-app.get('/register', (req, res) => {
-    res.render('register');
-});
-
-app.post('/register', async (req, res) => {
-    try {
-        const { nome, email, cpf, dataNascimento, password } = req.body;
-
-        const userExists = await User.findOne({ where: { cpf } });
-        if (userExists) {
-            return res.render('register', { error: 'Usuário já existe.' });
-        }
-
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        const newUser = await User.create({
-            nome,
-            email,
-            cpf,
-            dataNascimento,
-            password: hashedPassword
-        });
-
-        res.redirect('/login');
-    } catch (error) {
-        console.error('Erro ao criar usuário:', error);
-        res.render('register', { error: 'Erro ao criar o usuário. Por favor, tente novamente.' });
+// Rota principal (redirecionamento para login)
+app.get('/', (req, res) => {
+    if (req.isAuthenticated()) {
+        res.redirect('/trocavagas'); // Redireciona para a página principal do sistema (ou outra página)
+    } else {
+        res.redirect('/auth/login'); // Redireciona para a página de login se o usuário não estiver autenticado
     }
 });
 
-// Iniciar o servidor
 app.listen(PORT, () => {
     console.log(`O Express está rodando na porta ${PORT}`);
 });
-
-
