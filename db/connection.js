18b7cdca4509d1560bd098a77da62fab4f2a4577
Paulo Henrique Lima Leocádia <paulopitarac@gmail.com>
2023-11-26 20:09:04 -0300

    modificação

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   protocol: 'postgres',
   dialectOptions: {
     ssl: {
-      require: true,
+      require: false,
       rejectUnauthorized: false
     }
   }
