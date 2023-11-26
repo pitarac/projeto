44ba344cd3f56b9c13135733866f1db8bb38b0f4
Paulo Henrique Lima Leocádia <39973636+pitarac@users.noreply.github.com>
2023-11-26 01:32:31 -0300

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,26 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// Configuração para o PostgreSQL
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
+const { DATABASE_URL, NODE_ENV } = process.env;
+
+let sequelize;
+
+if (NODE_ENV === 'production') {
+  sequelize = new Sequelize(DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
-  }
-});
+  });
+} else {
+  sequelize = new Sequelize(DATABASE_URL, {
+    dialect: 'postgres'
+    // Outras opções para ambientes de desenvolvimento, se necessário
+  });
+}
 
 module.exports = sequelize;
