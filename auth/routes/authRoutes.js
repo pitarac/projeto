200c7e075886819a136a5949c8cdf06d1a137aee
Paulo Henrique Lima Leocádia <39973636+pitarac@users.noreply.github.com>
2023-11-26 01:21:05 -0300

    Create authRoutes.js

---
+++ auth/routes/authRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../auth/controllers/authController');
+
+// Rota para registro de novo usuário
+router.post('/register', async (req, res) => {
+    const { cpf, email, birthDate, password } = req.body;
+
+    try {
+        const newUser = await authController.registerUser(cpf, email, birthDate, password);
+        res.status(201).json({ message: 'Usuário registrado com sucesso.', user: newUser });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// Rota para autenticação de usuário
+router.post('/login', authController.authenticateUser);
+
+// Rota para logout de usuário
+router.get('/logout', authController.logoutUser);
+
+module.exports = router;
