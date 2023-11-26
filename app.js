d5476f54d7874784a306893fb2937c6b783f4021
Paulo Henrique Lima Leocádia <39973636+pitarac@users.noreply.github.com>
2023-11-26 01:45:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,19 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const exphbs = require('express-handlebars');
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcryptjs');
 const session = require('express-session');
 const bodyParser = require('body-parser');
-const Sequelize = require('sequelize');
 const db = require('./db/connection');
-const User = require('./models/User');
-const path = require('path');
+const authRoutes = require('./auth/routes/authRoutes');
+const userRoutes = require('./users/routes/userRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-require('./models/modelAssociations');
 
 // Configuração da sessão
 app.use(session({
@@ -27,61 +22,21 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Configuração da estratégia local do Passport
-passport.use(new LocalStrategy({ usernameField: 'cpf' }, async (cpf, password, done) => {
-    try {
-        const user = await User.findOne({ where: { cpf } });
-
-        if (!user) {
-            return done(null, false, { message: 'Usuário não encontrado.' });
-        }
-
-        const isMatch = await bcrypt.compare(password, user.password);
-
-        if (isMatch) {
-            return done(null, user);
-        } else {
-            return done(null, false, { message: 'Senha inválida.' });
-        }
-    } catch (error) {
-        return done(error);
-    }
-}));
-
-// Serialização e desserialização do usuário para sessões
-passport.serializeUser((user, done) => {
-    done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-    try {
-        const user = await User.findByPk(id);
-        done(null, user);
-    } catch (error) {
-        done(error, null);
-    }
-});
-
 // Body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Configuração do Handlebars
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-// Pasta estática
-app.use(express.static(path.join(__dirname, 'public')));
+// Rotas
+app.use('/auth', authRoutes);
+app.use('/user', userRoutes);
 
 // Conexão com o banco de dados
 db.authenticate()
-    .then(() => console.log('Conectou ao banco com sucesso'))
-    .catch(err => console.log('Ocorreu um erro ao conectar', err));
-
-// Rotas
-require('./routes/auth')(app);
-const trocavagasRoutes = require('./routes/trocavagas');
-app.use('/trocavagas', trocavagasRoutes);
+    .then(() => console.log('Conexão com o banco de dados estabelecida com sucesso'))
+    .catch(err => console.error('Erro ao conectar ao banco de dados:', err));
 
 // Rota para a raiz do domínio
 app.get('/', (req, res) => {
