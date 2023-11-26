9c4dfe061d0a69b7e8c8ea4eb425fc7403afcdb4
Paulo Henrique Lima Leocádia <39973636+pitarac@users.noreply.github.com>
2023-11-26 15:25:11 -0300

    Create passport-config.js

---
+++ auth/config/passport-config.js
@@ -0,0 +1,41 @@
+// auth/passport-config.js
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
+passport.use(new LocalStrategy(
+  async (cpf, password, done) => {
+    try {
+      const user = await User.findOne({ where: { cpf } });
+      if (!user) {
+        return done(null, false, { message: 'CPF não encontrado.' });
+      }
+      
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return done(null, false, { message: 'Senha incorreta.' });
+      }
+      
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findByPk(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+module.exports = passport;
