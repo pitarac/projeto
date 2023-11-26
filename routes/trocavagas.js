6b604a33e7fd1f2be915ff07882b58f2ae931358
Paulo Henrique Lima Leocádia <39973636+pitarac@users.noreply.github.com>
2023-11-25 22:01:33 -0300

    Update trocavagas.js

--- routes/trocavagas.js
+++ routes/trocavagas.js
@@ -1,65 +1,82 @@
-const express   = require('express');
-const router    = express.Router();
+const express = require('express');
+const router = express.Router();
 const Trocavaga = require('../models/Trocavaga');
 
-
-// rota de teste
+// Rota para teste
 router.get('/test', (req, res) => {
-  res.send('deu certo');
+    res.send('deu certo');
 });
 
+// Detalhe da vaga -> view/1, view/2
+router.get('/view/:id', (req, res) => {
+    Trocavaga.findOne({
+        where: { id: req.params.id }
+    })
+        .then(trocavaga => {
+            res.render('view', {
+                Trocavaga: trocavaga
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send('Erro ao encontrar a vaga');
+        });
+});
 
-// detalhe da vaga -> view/1, view/2
-router.get('/view/:id', (req, res) => Trocavaga.findOne({
-  where: {id: req.params.id}
-}).then(Trocavaga => {
-
-  res.render('view', {
-    Trocavaga
-  });
-
-}).catch(err => console.log(err)));
-
-
-// form da rota de envio
+// Formulário de envio
 router.get('/add', (req, res) => {
-  res.render('add');
-})
-
-// form da rota de envio
-router.get('/profile', (req, res) => {
-  res.render('profile');
-})
-
-// form da rota de envio
-router.get('/login', (req, res) => {
-  res.render('login');
-})
-
-// add vaga via post
-router.post('/add', (req, res) => {
-
-  let {responsavel, email, telefone, regiao_origem, escola_origem, grau_instrucao, serie_ano, turno_origem, regiao_destino, escola_destino, turno_destino, new_trocavaga} = req.body;
-
-  // insert
-  Trocavaga.create({
-    responsavel,
-    email,
-    telefone,
-    regiao_origem,
-    escola_origem,
-    grau_instrucao,
-    serie_ano,
-    turno_origem,
-    regiao_destino,
-    escola_destino,
-    turno_destino,
-    new_trocavaga
-  })
-  .then(() => res.redirect('/'))
-  .catch(err => console.log(err));
-
+    res.render('add');
 });
 
-module.exports = router
+// Página de perfil
+router.get('/profile', (req, res) => {
+    res.render('profile');
+});
 
+// Página de login (atente-se para a correção do nome da rota)
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+// Adicionar vaga via POST
+router.post('/add', (req, res) => {
+    const {
+        responsavel,
+        email,
+        telefone,
+        regiao_origem,
+        escola_origem,
+        grau_instrucao,
+        serie_ano,
+        turno_origem,
+        regiao_destino,
+        escola_destino,
+        turno_destino,
+        new_trocavaga
+    } = req.body;
+
+    // Inserir no banco de dados
+    Trocavaga.create({
+        responsavel,
+        email,
+        telefone,
+        regiao_origem,
+        escola_origem,
+        grau_instrucao,
+        serie_ano,
+        turno_origem,
+        regiao_destino,
+        escola_destino,
+        turno_destino,
+        new_trocavaga
+    })
+        .then(() => {
+            res.redirect('/');
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send('Erro ao adicionar a vaga');
+        });
+});
+
+module.exports = router;
