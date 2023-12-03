7e2157581b7d91fc693d45a2df18b05c6fa02c75
Paulo Henrique Lima Leocádia <paulopitarac@gmail.com>
2023-12-03 16:31:51 -0300

    det

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(session({
     cookie: {maxAge: 70 * 60 * 1000 },
    // store: 
 }));
-
+app.use(methodOverride('_method'));
 app.use((req, res, next) => {
     console.log('Estado da sessão:', req.session);
     next();
@@ -44,7 +44,7 @@ app.use((req, res, next) => {
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(methodOverride('_method'));
+
 
 app.use(messagesMiddleware);
 
